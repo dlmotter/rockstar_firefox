67c105d45a7064a3db3ea588ef5249d76d1ac4d5
Daniel Motter <dlmotter@gmail.com>
2026-01-07 10:41:43 -0500

    Add files via upload

--- rockstar.js
+++ rockstar.js
@@ -1600,8 +1600,7 @@
             }
         }
         
-        const popup = $(`<div class='rockstar-popup' style='position: absolute; z-index: 1000; left: ${position.left}; top: ${position.top}; max-height: calc(100% - 28px); max-width: calc(100% - 28px); padding: 8px; margin: 4px; overflow: auto; ` +
-                `background-color: white; border: 1px solid #ddd;'>` +
+        const popup = $(`<div class='rockstar-popup' style='position: absolute; z-index: 1000; left: ${position.left}; top: ${position.top}; max-height: calc(100% - 28px); max-width: calc(100% - 28px); padding: 8px; margin: 4px; overflow: auto;'>` +
                 `${main ? "<span class=title><span style='font-size: 18px;'>≡</span> " : "<span style='font-weight: bold'>"}${title}</span>` +
                 `<div class='rockstarButtons' style='display: block; float: right;'>${main ? "<span class=minimize style='padding: 4px'> _ </span>" : ""} ` + 
                 (main ? `<a class='whatsNew'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-wclassth="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" /></svg></a>` : '') +
